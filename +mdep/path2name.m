cdb5f38b8c3c2d6632e0b3d22b099e4c80a85381
George Kaplan <george@georgekaplan.xyz>
2018-05-16 12:35:43 -0400

    fixed indentation in path2name

--- +mdep/path2name.m
+++ +mdep/path2name.m
@@ -1,11 +1,11 @@
 function name = path2name(fullpth)
-    [p, f, e] = fileparts(fullpth);
-    [p2, d] = fileparts(p);
-    if d(1) == '@'
-      name = d;
-    elseif strcmp(e, '.m')
-      name = [f, e];
-    else
-      name = fullpth;
-    end
+  [p, f, e] = fileparts(fullpth);
+  [p2, d] = fileparts(p);
+  if d(1) == '@'
+    name = d;
+  elseif strcmp(e, '.m')
+    name = [f, e];
+  else
+    name = fullpth;
+  end
 end
