518bde6be1965063fc094a7abe16f16c8aab0eb0
George Kaplan <george@georgekaplan.xyz>
2018-05-16 12:34:50 -0400

    added special handling for packages to parsewhat.m

--- +mdep/parsewhat.m
+++ +mdep/parsewhat.m
@@ -1,6 +1,23 @@
 function parsed = parsewhat(whatout)
+  % helper variable
   uo = {'UniformOutput', false};
+
+  % fix class names
   classes_fixed = cellfun(@(c) ['@', c], whatout.classes, uo{:});
-  packages_fixed = cellfun(@(p) ['+', p], whatout.packages, uo{:});
-  parsed = cat(1, packages_fixed, classes_fixed, whatout.m, whatout.mex);
+
+  % special handling for packages
+  package_contents = cellfun(@(p) meta.package.fromName(p), whatout.packages, uo{:});
+  package_functions = cellfun(@(c) {c.FunctionList.Name}, package_contents, uo{:});
+
+  pkg = cell(sum(cellfun(@numel, package_functions)), 1);
+  k = 1;
+  for l = 1:length(package_contents)
+    for m = 1:length(package_functions{l})
+      pkg{k} = sprintf('+%s%s%s.m', whatout.packages{l}, filesep, package_functions{l}{m});
+      k = k + 1;
+    end
+  end
+
+  % put it all together
+  parsed = cat(1, pkg, classes_fixed, whatout.m, whatout.mex);
 end
