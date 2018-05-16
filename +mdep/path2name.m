facc61233b036796839d68f4541bfc6d82a70a42
George Kaplan <george@georgekaplan.xyz>
2018-05-16 12:37:56 -0400

    added handling for package files to path2name.m

--- +mdep/path2name.m
+++ +mdep/path2name.m
@@ -3,6 +3,8 @@ function name = path2name(fullpth)
   [p2, d] = fileparts(p);
   if d(1) == '@'
     name = d;
+  elseif d(1) == '+'
+    name = [d(2:end), '.', f, e];
   elseif strcmp(e, '.m')
     name = [f, e];
   else
